0685b000bb1c363bdeb60704e77d40386790acce
overstimulation <146885536+overstimulation@users.noreply.github.com>
2025-04-01 19:29:53 +0200

    20-99-bottles

---
+++ 4-loops/20-99-bottles.py
@@ -0,0 +1,7 @@
+for i in reversed(range(100)):
+  if i == 0:
+    print('No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.')
+  else:
+    print(f'{i} bottles of beer on the wall')
+    print(f'{i} bottles of beer')
+    print(f'Take one down, pass it around')
