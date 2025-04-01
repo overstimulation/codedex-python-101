7faaf101a59dc677d0af459a2ddc9e5da466a0f9
overstimulation <146885536+overstimulation@users.noreply.github.com>
2025-04-01 19:29:08 +0200

    18-guess-number

---
+++ 4-loops/18-guess-number.py
@@ -0,0 +1,11 @@
+guess = 0
+tries = 0
+
+while guess != 6 and tries < 5:
+  guess = int(input("Guess the number:  "))
+  tries += 1
+
+if tries > 5:
+  print('You didn\'t get it.')
+else:
+  print('You got it!')
