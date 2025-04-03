f736cb84652d777f6eecca2262dd9818495703d1
overstimulation <146885536+overstimulation@users.noreply.github.com>
2025-04-03 19:45:40 +0200

    24-inventory

---
+++ 5-lists/24-inventory.py
@@ -0,0 +1,3 @@
+lego_parts = [8980, 7323, 5343, 82700, 92232, 1203, 7319, 8903, 2328, 1279, 679, 589]
+print(f'The elves are running low on: {min(lego_parts)}')
+print(f'The elves are overstocking on: {max(lego_parts)}')
