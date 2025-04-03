d4ee3a24b395f880e5625421b689d599a7f0ceff
overstimulation <146885536+overstimulation@users.noreply.github.com>
2025-04-03 20:35:51 +0200

    27-bucket-list

---
+++ 5-lists/27-bucket-list.py
@@ -0,0 +1,11 @@
+things_to_do = [
+  '💻 Finish every course on Codédex.',
+  '🏙️ Live in a different city for a year.',
+  '🎵 Organise my music albums.',
+  '📚 Read every book I own.',
+  '🏆 Reach 1000 commits in a year on GitHub.',
+  '🚀 Grow old with no regrets.'
+]
+
+for i in things_to_do:
+    print(i)
