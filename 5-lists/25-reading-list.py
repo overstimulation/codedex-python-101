c1438209b13069cbdc389d5e29c7e4960ec9e3bf
overstimulation <146885536+overstimulation@users.noreply.github.com>
2025-04-03 20:25:37 +0200

    25-reading-list

---
+++ 5-lists/25-reading-list.py
@@ -0,0 +1,5 @@
+books = ['Harry Potter', '1984', 'The Fault in Our Stars', 'The Mom Test', 'Life in Code']
+books.append('Pachinko')
+books.remove('The Fault in Our Stars')
+books.pop(1)
+print(books)
