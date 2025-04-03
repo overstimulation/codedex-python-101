aa91415e9796ba26fd2a9a37b0bf054c5e1f99b1
overstimulation <146885536+overstimulation@users.noreply.github.com>
2025-04-03 19:42:33 +0200

    23-to-do

---
+++ 5-lists/23-to-do.py
@@ -0,0 +1,11 @@
+todo = ['🏦 Get quarters.',
+        '🧺 Do laundry.',
+        '🌳 Take a walk.',
+        '💈 Get a haircut.',
+        '🍵 Make some tea.',
+        '💻 Complete Lists chapter.',
+        '💖 Call mom.',
+        '📺 Watch My Hero Academia.']
+
+print(todo[1], todo[2])
+print(todo[3:6])
