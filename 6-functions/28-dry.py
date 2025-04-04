30eee7eaac0042991d3a2fe2279f88a0ab77922e
overstimulation <146885536+overstimulation@users.noreply.github.com>
2025-04-04 20:52:59 +0200

    28-dry

---
+++ 6-functions/28-dry.py
@@ -0,0 +1,8 @@
+# Find 4 built-in functions that we have used previously in the course.
+print('Hello World!') # Prints the given string to the console.
+print(input('Enter your text here! ')) # Prompts the user for input and returns it as a string.
+print(len([1,2,3,4,5])) # Returns the number of items in a sequence (like a list).
+print(int('123')) # Converts a value (like a string) to an integer.
+
+# Pick 1 built-in function that we have not seen before.
+print(bin(128)) # Converts an integer to its binary representation (as a string).
