64a7907e22a605b974a133630c9c284910a53603
overstimulation <146885536+overstimulation@users.noreply.github.com>
2025-04-09 00:43:47 +0200

    42-forty-two

---
+++ 8-modules/42-forty-two.py
@@ -0,0 +1,5 @@
+import wikipedia
+
+print(wikipedia.summary('Karnaugh map', sentences=2))
+print(wikipedia.summary('Quine–McCluskey algorithm', sentences=2))
+print(wikipedia.summary('Raven paradox', sentences=2))
